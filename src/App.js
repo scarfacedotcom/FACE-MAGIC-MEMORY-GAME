0b332403b4b657e38be21750d6eacde17684710f
scarfacedotcom <peteradaaku@gmail.com>
2022-06-18 09:04:36 +0100

    added the flipped property and matched cards code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import SingleCard from './components/SingleCard'
 
 const cardImages = [
-  { "src": "/img/khadi.jpg" },
-  { "src": "/img/longs.jpg" },
-  { "src": "/img/face-1.jpg" },
-  { "src": "/img/mercy.jpg" },
-  { "src": "/img/satoshi.jpg" },
-  { "src": "/img/scar.jpg" },
+  { "src": "/img/khadi.jpg"   , matched : false},
+  { "src": "/img/longs.jpg"   , matched : false},
+  { "src": "/img/face-1.jpg"  , matched : false},
+  { "src": "/img/mercy.jpg"   , matched : false},
+  { "src": "/img/satoshi.jpg" , matched : false},
+  { "src": "/img/scar.jpg"    , matched : false},
 ]
+
 
 function App() {
   const [cards, setCards] = useState([])
@@ -17,20 +18,44 @@ function App() {
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
 
-  //shuffle cards
+  // shuffle cards for new game
   const shuffleCards = () => {
-    const shuffleCards = [...cardImages, ...cardImages]
+    const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
-      .map((card) => ({...card, id: Math.random() }))
-
-    setCards(shuffleCards)
+      .map(card => ({ ...card, id: Math.random() }))
+      
+    setCards(shuffledCards)
     setTurns(0)
   }
 
   // handle a choice
   const handleChoice = (card) => {
-    choiceOne ? setChoiceTwo(card) : setChoiceTwo(card)
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
   }
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        resetTurn()
+      }
+
+    }
+  }, [choiceOne, choiceTwo])
+
+  console.log(cards)
 
   // reset choices & increase turn
   const resetTurn = () => {
@@ -41,18 +66,20 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Face Magic Match</h1>
+      <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
 
       <div className="card-grid">
         {cards.map(card => (
           <SingleCard 
-            key={card.id} 
+            key={card.id}
             card={card}
             handleChoice={handleChoice}
+            flipped ={card === choiceOne || card === choiceTwo || card.matched}
           />
         ))}
       </div>
+
     </div>
   );
 }
